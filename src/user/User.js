7b168840127c4a28a920b972901ab898b870a411
Ali Edalat <ali.edalat.77@gmail.com>
2019-04-10 12:18:50 -0700

    Add skill hover.

--- src/user/User.js
+++ src/user/User.js
@@ -4,17 +4,23 @@ import './user_profile.css'
 class User extends Component {
 	constructor(props){
         super(props);
-        this.state = {loaded: false, skills: []}
+        const { match: { params } } = this.props;
+        this.state = {loaded: false, skills: [], login: false, id: params.userId, hover: {enable: false, name: "", class: ""}}
         this.fetchData = this.fetchData.bind(this);
+        this.hoverOn = this.hoverOn.bind(this);
+        this.hoverOff = this.hoverOff.bind(this);
     }
 
     fetchData(){
-    	const { match: { params } } = this.props;
         const axios = require('axios');
-		axios.get(`http://localhost:8081/IERIA/user/${params.userId}`)
+		axios.get(`http://localhost:8081/IERIA/user/${this.state.id}`)
     	.then(({ data: user }) => {
       		this.setState({ user });
       		this.setState({loaded: true});
+      		if(this.state.id == '1'){
+	        	this.setState({login: true})
+	        }
+	        console.log(this.state)
     	}).catch(error => this.setState({ error, loaded: true }));
     }
 
@@ -22,8 +28,21 @@ class User extends Component {
 		this.fetchData()
     }
 
+    hoverOn(keyName){
+    	this.setState({hover: {enable: true, name: keyName, class: "badge badge-success ml-1 my-0 font py-1"}})
+    	console.log(keyName)
+    }
+
+    hoverOff(keyName){ 
+    	this.setState({hover: {enable: false, name: "", class: ""}})
+    }
+
+    handleClick(){
+    	
+    }
+
 	render() {
-		console.log(this.state.skills)
+		console.log(this.state)
 	    return (
 	    	<React.Fragment>
 	        <div class="white_bar">
@@ -43,10 +62,10 @@ class User extends Component {
 	                    <div class="col-9">
 	                        <ul class="name_text font">
 	                            <li class="full_name">
-	                                محمدرضاکیانی
+	                                {this.state.user.firstName + ' ' + this.state.user.lastName}
 	                            </li>
 	                            <li class="nickname">
-	                                اعلی حضرت
+	                                {this.state.user.jobTitle}
 	                            </li>
 	                        </ul>
 	                    </div>
@@ -60,12 +79,38 @@ class User extends Component {
 	                        </ul>
 	                    </div>
 	                </div>
+	                {this.state.login ?(
+	                <div class="row">
+			            <div class="col-12">
+			                <div class="float-right">
+			                    <div class="d-flex align-items-baseline mr-4">
+			                    	<div class="description_skills font mr-1">
+			                    		<b>
+			                            	مهارت ها:
+			                            </b>
+			                        </div>
+			                        <div class="card bg-light pl-1 mr-2 shadow-sm px-1 py-1">
+			                            <form action="" method="" class="white_bar">
+			                                <select name="" class="bg-light description_skills font pl-5 py-1">
+			                                    <option value="-- انتخاب مهارت --">-- انتخاب مهارت --</option>
+			                                    <option value="CSS" dir="ltr">CSS</option>
+			                                    <option value="C++" dir="ltr">C++</option>
+			                                </select>
+			                                <button class="btn btn-primary description_skills font mr-1">افزودن مهارت</button>
+			                            </form>
+			                        </div>
+			                    </div>
+			                </div>
+			            </div>
+			        </div>
+			        ):(null)}
 	                <div class="row font float-left ml-4">
 	                    {Object.keys(this.state.user.skills).map((keyName, i) => (
 							<div id={keyName} class="col-auto">
             				<div class="bg-white shadow-sm px-1 font rounded-top rounded-bottom"><span class={!(this.state.user.skills[keyName].endorsers.find(function(element) {
 							return element == '1'; // 1 is login user 
-							}))?"badge blue ml-1 my-0 font py-1 text-info":"badge badge-success ml-1 my-0 font py-1"}>{this.state.user.skills[keyName].point}</span>{keyName}</div></div>  
+							}))?(this.state.hover.enable && keyName == this.state.hover.name)?this.state.hover.class:"badge blue ml-1 my-0 font py-1 text-info":"badge badge-success ml-1 my-0 font py-1"} onClick={this.handleClick} onMouseEnter={() => this.hoverOn(keyName)} onMouseLeave={() => this.hoverOff(keyName)}>
+							<a href="#">{this.state.user.skills[keyName].point}</a></span>{keyName}</div></div>  
 						))}
 	                </div>
 	            </div>
